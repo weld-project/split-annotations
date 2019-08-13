6bad2332603b595fc4b69359f27b4d17b4f5955c
Shoumik Palkar <shoumik@cs.stanford.edu>
2019-08-13 10:23:17 -0700

    Minor fix for experiments requirements.txt

--- python/benchmarks/get-data.sh
+++ python/benchmarks/get-data.sh
@@ -21,6 +21,7 @@ rm ml-1m.zip
 cd $TOP
 cd birth_analysis
 gunzip -k babynames.txt.gz
+rm -rf _data
 mkdir -p _data
 mv babynames.txt _data
 ./replicate-csv -i _data/babynames.txt -o  _data/babynames-xlarge.txt -r 80
