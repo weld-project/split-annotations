50e906ac07cbc73bcb8ec80cefe2a001d3ff7ba9
Shoumik Palkar <shoumik@cs.stanford.edu>
2019-09-20 13:56:02 -0700

    Start better pandas annotations

--- python/benchmarks/data_cleaning/data_cleaning.py
+++ python/benchmarks/data_cleaning/data_cleaning.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append("../../lib")
 sys.path.append("../../pycomposer/")
 
-import composer_pandas as pd
+import sa.annotated.pandas as pd
 
 def gen_data(size):
     values = ["1234567" for  _ in range(size)]
@@ -18,6 +18,7 @@ def gen_data(size):
 
 def datacleaning_pandas(requests):
     requests = requests.str.slice(0, 5)
+    print(type(requests))
     zero_zips = requests == "00000"
     requests = requests.mask(zero_zips, np.nan)
     requests = requests.unique()
@@ -42,7 +43,7 @@ def run():
     parser = argparse.ArgumentParser(
         description="Data Cleaning"
     )
-    parser.add_argument('-s', "--size", type=int, default=26, help="Size of each array")
+    parser.add_argument('-s', "--size", type=int, default=10, help="Size of each array")
     parser.add_argument('-p', "--piece_size", type=int, default=16384*2, help="Size of each piece.")
     parser.add_argument('-t', "--threads", type=int, default=1, help="Number of threads.")
     parser.add_argument('-v', "--verbosity", type=str, default="none", help="Log level (debug|info|warning|error|critical|none)")
--- python/pycomposer/sa/annotated/pandas/__init__.py
+++ python/pycomposer/sa/annotated/pandas/__init__.py
@@ -4,3 +4,40 @@ from pandas import *
 
 from .annotated import *
 from sa.annotation import evaluate
+
+class SeriesSplit(SplitType):
+    def combine(self, values):
+        return concat(values)
+
+    def split(self, start, end, value):
+        return value[start:end]
+
+Series.abs =  sa((SeriesSplit(),), {}, SeriesSplit())(Series.abs)
+
+Series.add = sa((SeriesSplit(), SeriesSplit()), {}, SeriesSplit())(Series.add)
+Series.all = sa((SeriesSplit(),), {}, AllSplit())(Series.all)
+Series.any = sa((SeriesSplit(),), {}, AnySplit())(Series.any)
+Series.apply = sa((SeriesSplit(), Broadcast()), {}, SeriesSplit())(Series.apply)
+
+Series.between = sa((SeriesSplit(), Broadcast(), Broadcast()), {}, SeriesSplit())(Series.between)
+Series.between_time = sa((SeriesSplit(), Broadcast(), Broadcast()), {}, SeriesSplit())(Series.between_time)
+
+Series.bfill = sa((SeriesSplit(), ), {}, SeriesSplit())(Series.bfill)
+Series.clip = sa((SeriesSplit(),), {}, SeriesSplit())(Series.clip)
+Series.combine = sa((SeriesSplit(), SeriesSplit()), {}, SeriesSplit())(Series.combine)
+Series.combine_first = sa((SeriesSplit(), SeriesSplit()), {}, SeriesSplit())(Series.combine_first)
+
+Series.div = sa((SeriesSplit(), SeriesSplit()), {}, SeriesSplit())(Series.div)
+Series.divide = sa((SeriesSplit(), SeriesSplit()), {}, SeriesSplit())(Series.divide)
+Series.divmod = sa((SeriesSplit(), SeriesSplit()), {}, SeriesSplit())(Series.divmod)
+
+Series.dot = sa((SeriesSplit(), SeriesSplit()), {}, DotSplit())(Series.dot)
+
+Series.drop_duplicates = sa((SeriesSplit(),), {}, DotSplit())(Series.drop_duplicates)
+Series.dropna = sa((SeriesSplit(),), {}, DotSplit())(Series.dropna)
+
+Series.eq = sa((SeriesSplit(), SeriesSplit()), {}, EqSplit())(Series.eq)
+Series.equals = sa((SeriesSplit(), SeriesSplit()), {}, EqualsSplit())(Series.equals)
+
+Series.ffill = sa((SeriesSplit(), ), {}, SeriesSplit())(Series.ffill)
+Series.floordiv = sa((SeriesSplit(), SeriesSplit()), {}, SeriesSplit())(Series.floordiv)
